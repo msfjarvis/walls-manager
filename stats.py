e93bfa84892d341577cf114f18d870f75e7716e8
Harsh Shandilya <harsh@prjkt.io>
2018-09-27 22:31:24 +0530

    Add directory size to stats
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- stats.py
+++ stats.py
@@ -1,10 +1,10 @@
 import os
+import math
 
 picture_stats = {}
 
 
 def walk_dir(directory='.', extension='jpg'):
-    global total_count
     total_count = 0
     extension = extension.lower()
     for dirpath, dirnames, files in os.walk(directory):
@@ -15,14 +15,35 @@ def walk_dir(directory='.', extension='jpg'):
                 model_name = sanitized_name.replace("_{}".format(count), "")
                 picture_stats[model_name] = picture_stats.get(model_name, 0) + 1
                 total_count += 1
+    return total_count
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
+
+def calc_size(directory='.'):
+    total_size = 0
+    for dirpath, dirnames, files in os.walk(directory):
+        for f in files:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return convert_size(total_size)
 
 
 def parse_and_display_stats(directory='.'):
-    walk_dir(directory)
+    total_count = walk_dir(directory)
     print("Directory analysis for {}\n".format(directory))
     for key, value in sorted(picture_stats.items()):
         print("{}: {}".format(key.replace("_", " "), value))
-    print("\nTotal: {}".format(total_count))
+    print("\nTotal count: {}".format(total_count))
+    print("Total size: {}".format(calc_size(directory)))
 
 
 if __name__ == '__main__':
