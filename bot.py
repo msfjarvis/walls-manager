b07421515151ec844a4db4b36a6df36588998c6a
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-22 23:10:33 +0530

    bot.py: Fix pylint problems
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python3
+# pylint: disable=missing-docstring
+
 import logging
 import configparser
 
 from telegram.ext import Updater, CommandHandler
 from search import search_files
 
-config = configparser.ConfigParser()
+config = configparser.ConfigParser()  # pylint: disable=invalid-name
 config.read("config.ini")
 TOKEN = config["BOT"]["TOKEN"]
 LOCAL_DIR = config["SOURCE"]["DIR"]
@@ -24,15 +27,14 @@ def search(bot, update, args):
     else:
         message = "Results for '{}':\n".format(pretty_name)
         for item in iter(sorted(found_files)):
-            message += "[{}]({}/{})\n".format(item, REMOTE_URL, item)
+            message += "[{0}]({1}/{0})\n".format(item, REMOTE_URL)
 
         bot.send_message(update.message.chat_id, message, "Markdown", disable_web_page_preview=True)
 
 
 def main():
     updater = Updater(TOKEN)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("search", search, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler("search", search, pass_args=True))
     updater.start_polling()
     updater.idle()
 
