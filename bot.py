82d62f80b197474479717f21685cc63cf810f357
Harsh Shandilya <msfjarvis@gmail.com>
2019-01-31 15:08:06 +0530

    bot: Re-do logging setup
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- bot.py
+++ bot.py
@@ -21,9 +21,6 @@ LOCAL_DIR = config["SOURCE"]["DIR"]
 REMOTE_URL = config["DEST"]["PUBLIC_URL"]
 PHOTO_SIZE_THRESHOLD = 5242880  # 5mB, from Telegram documentation.
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
-
 
 @send_action(ChatAction.TYPING)
 def search(bot, update, args):
@@ -106,9 +103,18 @@ def capitalize(string):
     return "".join(chars)
 
 
-def main():
+def configure_logging():
     if os.getenv("DEBUG", "") != "":
-        logging.basicConfig(filename="log.log")
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.DEBUG,
+                            filename="log.log")
+
+
+def main():
+    configure_logging()
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("search", search, pass_args=True))
