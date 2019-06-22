584c627c0d4333738ddd84de118e8c68d971953e
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-22 16:56:25 +0530

    search: More matching improvements
    - Ensure we properly discard bad matches
    - Match search terms to start of every name entity
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- search.py
+++ search.py
@@ -2,6 +2,7 @@
 # pylint: disable=missing-docstring
 
 import os
+from os.path import splitext
 from typing import List
 
 
@@ -9,6 +10,8 @@ def search_files(file_name: str, directory: str = '.') -> List[str]:
     found_files = []
     for _, _, files in os.walk(directory):
         for name in files:
-            if file_name in name.lower().replace("_", " "):
-                found_files.append(name)
+            items = splitext(name.lower())[0].split("_")
+            for item in items:
+                if item == file_name or item.startswith(file_name):
+                    found_files.append(name)
     return found_files
