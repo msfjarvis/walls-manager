35d96be485cd1f4a0064f4430a43d069aa883bf5
Harsh Shandilya <msfjarvis@gmail.com>
2019-06-25 21:44:28 +0530

    Revert "search: More matching improvements"
    Breaks down under some conditions
    
    This reverts commit 584c627c0d4333738ddd84de118e8c68d971953e.
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- search.py
+++ search.py
@@ -2,7 +2,6 @@
 # pylint: disable=missing-docstring
 
 import os
-from os.path import splitext
 from typing import List
 
 
@@ -10,8 +9,6 @@ def search_files(file_name: str, directory: str = '.') -> List[str]:
     found_files = []
     for _, _, files in os.walk(directory):
         for name in files:
-            items = splitext(name.lower())[0].split("_")
-            for item in items:
-                if item == file_name or item.startswith(file_name):
-                    found_files.append(name)
+            if file_name in name.lower().replace("_", " "):
+                found_files.append(name)
     return found_files
